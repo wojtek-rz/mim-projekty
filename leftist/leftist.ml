937086528b2d540dbf75e027d2abcde70eccbe0c
Littleswift <w.r.mirror@gmail.com>
2021-11-10 19:16:48 +0100

    ostatnie zmiany

--- leftist/leftist.ml
+++ leftist/leftist.ml
@@ -1,10 +1,17 @@
-(* wersja z 27.10 23:00*)
+(* 		                                            *)
+(* 		    autor: Wojciech Rzepliński (438709)     *)
+(*		    reviewer: Adam Greloch (438473)         *)
+
+
+(* typ queue zawiera lewe podrzewo, priorytet, prawe poddrzewo, minimalną wysokość *)
 type 'a queue = Leaf | Node of 'a queue * 'a * 'a queue * int;;
 
-let empty = Leaf;;
+let empty = Leaf
+;;
 let get_height = function
 	| Node(_,_,_,h) -> h
 	| Leaf -> -1
+;;
 let swap_if_necessary tree = 
 	let swap_subtrees tree = 
 		match tree with
@@ -20,17 +27,15 @@ let swap_if_necessary tree =
 let rec merge tree1 tree2 =
 	match tree1, tree2 with
 	| Leaf, t| t, Leaf -> t
-	| Node(left_subtree, priority1, right_subtree, min_h1), Node(lt2, priority2, rt2, min_h2)-> 
+	| Node(left_subtree, priority1, right_subtree, _), Node(_, priority2, _, _)-> 
 		if priority1>priority2 then merge tree2 tree1
-		else
-			(* lewy zawsze ma mniejszy priorytet *)
+		else (* lewy zawsze ma mniejszy priorytet *)
 			let new_right_subtree = merge right_subtree tree2
 			in let new_height = min (get_height new_right_subtree) (get_height left_subtree) + 1
 			in swap_if_necessary(Node(left_subtree, priority1, new_right_subtree, new_height))
 ;;
 
-let add a kolejka =
-	merge (Node(Leaf, a, Leaf, 0)) kolejka
+let add a kolejka = merge (Node(Leaf, a, Leaf, 0)) kolejka (*łączymy drzewo z jednym elementem z pozostalym drzewem*)
 ;;
 exception Empty;;
 let delete_min tree=
@@ -39,6 +44,4 @@ let delete_min tree=
 	| Node(lt, x, rt, h) -> x, merge lt rt
 ;;
 let join tree1 tree2 = merge tree1 tree2;;
-let is_empty tree = tree=Leaf;;
-
-(* let c = empty |> add 5 |> add 8 |> add 9 |> add 10 |> add 10 |> add 15 |> add 17 |> add 10 |> add 3 |> add 5 |> add 5;; *)
+let is_empty tree = (tree=Leaf);;
