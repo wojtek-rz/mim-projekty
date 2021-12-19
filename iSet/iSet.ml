17250f199284deb7a01b945bee487a64a0488c76
Littleswift <w.r.mirror@gmail.com>
2021-12-19 09:21:39 +0100

    topol zrobione

--- iSet/iSet.ml
+++ iSet/iSet.ml
@@ -1,227 +1,3 @@
-(*      autor: Wojciech Rzepliński  (438709)    *)
-(*   reviewer: Stanisław Bitner     (438247)    *)
-(*                                              *)
-(*  licencja GNU Lesser General Public na dole  *)
-
-(* type t consist of: left subtree, interval, right subtree, max height, number of elements in an interval and below in whole set*)
-type t =Empty | Node of t * (int*int) * t * int * int
-
-(*                       small helpers                       *)
-(* ----------------------------------------------------------*)
-let height = function
- | Node (_, _, _, h,_) -> h
- | Empty -> 0
-
-let size = function
-| Node (_, _, _,_, s) -> s
-| Empty -> 0
-
-(* to sum large values safely*)
-let my_sum a b =
-    if a>0 && b>0 && a+b<0 then max_int
-    else a+b;;
-let my_sum3 a b c=
-    my_sum c (my_sum a b)
-
-(* to calculate number of elements in a range safely*)
-let between_range (k1,k2) = 
-    if k1<=0 && k2>=0 && k2-k1+1<=0 then max_int (* if k1 > 0 then k2-k1+1 will always be lower than max_int, we don't have to check that*)
-    else k2-k1+1
-
-let cmp = (fun (x_start,x_end) (y_start,y_end) -> 
-    if y_start > x_end then -1 (* first one larger*)
-    else if y_end < x_start then 1 (*second one larger*)
-    else 0 (*share common part; ((4,5) (5,6) also do)*))
-(*                       large helpers                       *)
-(* ----------------------------------------------------------*)
-
-(*  Creates a new node with son l, value k and right son r.
-    all values in l < k < all values in r
-    l and r must be balanced*)
-let make l k r = Node (l, k, r, max (height l) (height r) + 1, my_sum3 (size l) (size r) (between_range k))
-
-(* balances tree*)
-let bal l k r =
- let hl = height l in
- let hr = height r in
- if hl > hr + 2 then
-   match l with
-   | Node (ll, lk, lr, _, _) ->
-       if height ll >= height lr then make ll lk (make lr k r)
-       else
-         (match lr with
-         | Node (lrl, lrk, lrr, _, _) ->
-             make (make ll lk lrl) lrk (make lrr k r)
-         | Empty -> assert false)
-   | Empty -> assert false
- else if hr > hl + 2 then
-   match r with
-   | Node (rl, rk, rr, _, _) ->
-       if height rr >= height rl then make (make l k rl) rk rr
-       else
-         (match rl with
-         | Node (rll, rlk, rlr, _, _) ->
-             make (make l k rll) rlk (make rlr rk rr)
-         | Empty -> assert false)
-   | Empty -> assert false
- else Node (l, k, r, max hl hr + 1, my_sum3 (between_range k) (size l) (size r))
-
- (*find minimum element*)
-let rec min_elt = function
- | Node (Empty, k, _, _, _) -> k
- | Node (l, _, _, _,_) -> min_elt l
- | Empty -> raise Not_found
-
-let rec remove_min_elt = function
- | Node (Empty, _, r, _,_) -> r
- | Node (l, k, r, _,_) -> bal (remove_min_elt l) k r
- | Empty -> invalid_arg "PSet.remove_min_elt"
-
- (* Merges two trees t1 and t2 into one
-    Assuming relative height between t1 and t2 <= 2 *)
-let merge t1 t2 =
- match t1, t2 with
- | Empty, _ -> t2
- | _, Empty -> t1
- | _ ->
-     let k = min_elt t2 in
-     bal t1 k (remove_min_elt t2)
-
-
-(*  Adds one interval to the rest of the tree
-    interval must be disjoint with all intervals in tree
-    function unchanged*)
-let rec add_one x = function
- | Node (l, k, r, h, s) ->
-     let c = cmp x k in
-     if c = 0 then Node (l, x, r, h, s) (*this should never happen*)
-     else if c < 0 then
-       let nl = add_one x l in
-       bal nl k r
-     else
-       let nr = add_one x r in
-       bal l k nr
- | Empty -> Node (Empty, x, Empty, 1, between_range x)
-
- (* Creates new tree with left subtree, v value and right subree.
-    All elements in l and r must satisfy l < v < r and subtress balanced
-    No assumptions on relative heights of trees*)
-let rec join l v r =
- match (l, r) with
-   (Empty, _) -> add_one v r
- | (_, Empty) -> add_one v l
- | (Node(ll, lv, lr, lh,_), Node(rl, rv, rr, rh,_)) ->
-     if lh > rh + 2 then bal ll lv (join lr v r) else
-     if rh > lh + 2 then bal (join l v rl) rv rr else
-     make l v r
-
-(* calculates maximum range of intervals that are intersect (x1,x2) at any moment*)
-let sum_when_intersecting (x1,x2) tree =
-    let rec find_max_r x2 = function
-        | Empty -> x2
-        | Node(l, (v1,v2), r, _,_) -> 
-            if (v1-1<=x2 || v1<=x2+1) && x2<=v2 then v2 (* this alternative means the same, but on max_int or min_int works only one*)
-            else if x2<v1 then find_max_r x2 l
-            else find_max_r x2 r
-    in let rec find_min_r x1 = function
-        | Empty -> x1
-        | Node(l, (v1,v2), r, _,_) -> 
-            if v1<=x1 && (x1-1<=v2||x1<=v2+1) then v1 (* to assure that it extend to v1 if: (v1, 4) and (5,_)*)
-            else if x1<v1 then find_min_r x1 l
-            else find_min_r x1 r
-    in (find_min_r x1 tree), (find_max_r x2 tree)
-
-(*                       interface functions                 *)
-(* ----------------------------------------------------------*)
-let empty = Empty
-let is_empty x = (x = Empty)
-
-(*  Splits tree into 2 subtrees l and r
-    all elements in l < x < all elements in r
-    resulting subtress are balanced (but their heights may be different)  *)
-let split x set =
- let rec loop x = function
-     Empty ->
-       (Empty, false, Empty)
-   | Node (l, (v1, v2), r, _,_) ->
-        let c = cmp (x,x) (v1,v2) in
-        if c = 0 then 
-            if v1=x && v2=x then (l, true, r)
-            else if v1=x then (l, true, add_one(x+1,v2) r)
-            else if v2=x then (add_one(v1, x-1) l, true, r)
-            else (add_one (v1, x-1) l, true, add_one(x+1,v2) r)
-        else if c < 0 then
-            let (ll, pres, rl) = loop x l 
-            in (ll, pres, join rl (v1,v2) r)
-        else
-            let (lr, pres, rr) = loop x r 
-            in (join l (v1,v2) lr, pres, rr)
-    in
-    let setl, pres, setr = loop x set in
-    setl, pres, setr
-
-(* removes range (x1,x2) from set, no assumptions are required*)
-let remove (x1, x2) set =
-        let (_, _, (rset)) = split x2 set
-        and ((lset), _, _) = split x1 set
-    in
-    if is_empty rset then lset
-    else if is_empty lset then rset
-    else 
-        join lset (min_elt rset) (remove_min_elt rset)
-
-
-(* adds interval (x1,x2) to set, no assupmtions needed*)
-let add (x1,x2) set =
-    let s1, s2 = sum_when_intersecting (x1,x2) set
-    in let nset = remove (s1,s2) set
-    in add_one (s1,s2) nset
-
-(* checks whether x is in set, almost unchanged*)
-let mem x set =
- let rec loop = function
-   | Node (l, k, r, _,_) ->
-       let c = cmp (x,x) k in
-       c = 0 || loop (if c < 0 then l else r)
-   | Empty -> false in
- loop set
-
-let exists = mem
-
-let iter f set =
- let rec loop = function
-   | Empty -> ()
-   | Node (l, k, r, _,_) -> loop l; f k; loop r in
- loop set
-
-let fold f set acc =
- let rec loop acc = function
-   | Empty -> acc
-   | Node (l, k, r, _,_) ->
-         loop (f k (loop acc l)) r in
- loop acc set
-
-let elements set = 
- let rec loop acc = function
-     Empty -> acc
-   | Node(l, k, r, _, s) -> loop (k :: loop acc r) l in
- loop [] set
-
-
- (* calculates number of values that are below x in a given set*)
- let below x set = 
-    let rec look x = function
-    | Node(l, (v1,v2), r, _, s) ->
-        let c = cmp (x,x) (v1,v2) in
-        if c=0 then 
-            my_sum (size l) (between_range (v1,x))
-        else if c<0 then
-            look x l
-        else my_sum3 (size l) (between_range (v1,v2)) (look x r)
-    | Empty -> 0
-    in look x set
-;;
-
 (*
  * PSet - Polymorphic sets
  * Copyright (C) 1996-2003 Xavier Leroy, Nicolas Cannasse, Markus Mottl
@@ -241,3 +17,222 @@ let elements set =
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
+
+ (*Kamil Pilkiewicz*)
+(*Code reviewer: Maja Tkaczyk*)
+
+(*typy*)
+
+type t = Empty | Node of t * (int * int) * t * int * int;;
+
+(*komparator*)
+let cmp = fun (lx, gx) (lv, gv) ->
+  if gx < lv then -1 else (* x x v v*)
+  if lx = lv && gx = gv then 0 else (* x = v x = v*)
+  if lv <= lx && gx <= gv then 1 else (* v x x v  *)
+  if lv <= lx && gv < gx then 2 else (* v x v x *)
+  if lx < lv && gx <= gv then 3 else (* x v x v *)
+  if lx < lv && gv < gx then 4 (* x v v x *)
+  else 5
+;;
+
+(*funkcje pomocnicze*)
+
+let add_carefully_interval acc (l, g) = 
+  if g - l + 1 <= 0 then max_int else
+  if acc + (g - l + 1) <= 0 then max_int else
+  acc + (g - l + 1)
+;;
+let add_carefully_integer acc x =
+  if acc + x < 0 then max_int else
+  acc + x
+;;
+
+let cut_out (lx, gx) (lv, gv) = (*zwraca liste przedzialow, jakie zostana, sa 3 mozliwosci: pusta lista, jeden przedzial, dwa rozlaczne przedzialy*)
+  if gx < lv || lx > gv then [(lv, gv)] else
+  if lv < lx && gx < gv then [(lv, lx - 1); (gx + 1, gv)] else
+  if gx < gv then [(gx + 1, gv)] else
+  if lv < lx then [(lv, lx - 1)] else
+  []
+;;
+
+let height = function (*juz git*)
+  | Node (_, _, _, h, _) -> h
+  | Empty -> 0
+;;
+
+let size = function
+  | Node(_, _, _, _, s) -> s
+  | Empty -> 0
+;;
+
+let make l k r = Node (l, k, r, max (height l) (height r) + 1, add_carefully_interval (add_carefully_integer (size l) (size r)) k);;
+
+let bal l k r =
+  let hl = height l in
+  let hr = height r in
+  if hl > hr + 2 then
+    match l with
+    | Node (ll, lk, lr, _, _) ->
+        if height ll >= height lr then make ll lk (make lr k r)
+        else
+          (match lr with
+          | Node (lrl, lrk, lrr, _, _) ->
+              make (make ll lk lrl) lrk (make lrr k r)
+          | Empty -> assert false)
+    | Empty -> assert false
+  else if hr > hl + 2 then
+    match r with
+    | Node (rl, rk, rr, _, _) ->
+        if height rr >= height rl then make (make l k rl) rk rr
+        else
+          (match rl with
+          | Node (rll, rlk, rlr, _, _) ->
+              make (make l k rll) rlk (make rlr rk rr)
+          | Empty -> assert false)
+    | Empty -> assert false
+  else make l k r
+;;
+
+let rec min_elt = function
+  | Node (Empty, k, _, _, _) -> k
+  | Node (l, _, _, _, _) -> min_elt l
+  | Empty -> raise Not_found
+;;
+
+let rec remove_min_elt = function 
+  | Node (Empty, _, r, _, _) -> r
+  | Node (l, k, r, _, _) -> bal (remove_min_elt l) k r
+  | Empty -> invalid_arg "PSet.remove_min_elt"
+;;
+
+let merge t1 t2 = 
+  match t1, t2 with
+  | Empty, _ -> t2
+  | _, Empty -> t1
+  | _ ->
+      let k = min_elt t2 in
+      bal t1 k (remove_min_elt t2)
+;;
+
+let rec simplified_add_one x = function
+  | Node (l, k, r, h, s) ->
+      let c = cmp x k in
+      if c = 0 then Node (l, x, r, h, s)
+      else if c < 0 then
+        let nl = simplified_add_one x l in
+        bal nl k r
+      else
+        let nr = simplified_add_one x r in
+        bal l k nr
+  | Empty -> Node (Empty, x, Empty, 1, add_carefully_interval 0 x)
+;;
+
+let rec max_lower_than_x (lx, gx) t =
+  match t with
+  | Empty -> lx
+  | Node(l, (lk, gk), r, _, _) ->
+      if gk < lx - 1 then max_lower_than_x (lx, gx) r else
+      if lk <= lx then lk else
+      max_lower_than_x (lx, gx) l
+;;
+
+let rec min_greater_than_x (lx, gx) t =
+  match t with
+  | Empty -> gx
+  | Node(l, (lk, gk), r, _, _) ->
+      if lk > gx + 1 then min_greater_than_x (lx, gx) l else
+      if gk >= gx then gk else
+      min_greater_than_x (lx, gx) r
+;;
+
+let rec join l v r =
+  match (l, r) with
+    (Empty, _) -> simplified_add_one v r
+  | (_, Empty) -> simplified_add_one v l
+  | (Node(ll, lv, lr, lh, _), Node(rl, rv, rr, rh, _)) ->
+      if lh > rh + 2 then bal ll lv (join lr v r) else
+      if rh > lh + 2 then bal (join l v rl) rv rr else
+      make l v r
+;;
+
+(*konstruktory*)
+
+let empty = Empty;;
+
+(*selektory*)
+let is_empty x = x = Empty;;
+
+(*modyfikatory*)
+
+let split x t =
+  let rec loop x = function
+      Empty ->
+        (Empty, false, Empty)
+    | Node (l, v, r, _, _) ->
+        let c = cmp (x, x) v in
+        if c = 0 then (l, true, r) else
+        if c = 1 then 
+          if fst v < x then 
+            if (snd v) > x then (simplified_add_one (fst v, x - 1) l, true, simplified_add_one (x + 1, snd v) r) else
+            (simplified_add_one (fst v, x - 1) l, true, r) else
+          (l, true, simplified_add_one (x + 1, snd v) r)
+        else if c < 0 then
+          let (ll, pres, rl) = loop x l in (ll, pres, join rl v r)
+        else
+          let (lr, pres, rr) = loop x r in (join l v lr, pres, rr)
+  in
+  let setl, pres, setr = loop x t in
+  setl, pres, setr
+;;
+
+let remove x t =
+  let (l, un1, un2) = split (fst x) t
+  and (un3, un4, r) = split (snd x) t in
+  if r = Empty then l else
+    let k = min_elt r in
+    join l k (remove_min_elt r)
+;;
+
+let rec add x t =
+  let fl = max_lower_than_x x t
+  and fr = min_greater_than_x x t in
+  simplified_add_one (fl, fr) (remove (fl, fr) t)
+;;
+
+let mem x t =
+  let rec loop = function
+    | Node (l, k, r, _, _) ->
+        let c = cmp (x, x) k in
+        c = 0 || c = 1 || loop (if c < 0 then l else r)
+    | Empty -> false in
+  loop t
+;;
+let iter f t = (*map_tree*)
+  let rec loop = function
+    | Empty -> ()
+    | Node (l, k, r, _, _) -> loop l; f k; loop r in
+  loop t
+;;
+let fold f t acc = (*fold_tree*)
+  let rec loop acc = function
+    | Empty -> acc
+    | Node (l, k, r, _, _) ->
+          loop (f k (loop acc l)) r in
+  loop acc t
+;;
+let elements t =
+  let rec loop acc = function
+      Empty -> acc
+    | Node(l, k, r, _, _) -> loop (k :: loop acc r) l in
+  loop [] t
+;;
+let below n t = 
+  let rec loop acc = function
+      Empty -> acc
+    | Node(l, k, r, _, _) ->
+        if fst k > n then loop acc l else
+          let interval = (fst k, min n (snd k)) in 
+          loop (add_carefully_interval (add_carefully_integer acc (size l)) interval) r in
+  loop 0 t
+;;
--- origami/origami.ml
+++ origami/origami.ml
@@ -10,55 +10,51 @@ type kartka = point -> int;;
 type gdzie = Prawo|Lewo|Na;;
 
 (*=================================== HELPER FUNCTIONS ===================================*)
-
 (* let fix_number f = floor (f *. 1.e14) /. (1.e14) *)
-let prawolewona (v1x,v1y) (v2x,v2y) (* given 2 vectors: [v1x, v1y] [v2x, v2y]*)=
-    let w = v1x *. v2y -. v1y *. v2x in (* calulate the cross product of them*)
+
+(* dla danych dwoch wektorow w formie [v1x,v1y] [v2x,x2y] decyduje, czy ten drugi znajduje sie
+po prawej stronie pierwszego, poprzez obliczenie ich iloczynu wektorowego i analizowanie jego znaku*)
+let prawo_lewo_na (v1x, v1y) (v2x, v2y) =
+    let w = v1x *. v2y -. v1y *. v2x in 
     if w < 0. then Prawo 
     else if w = 0. then Na
     else Lewo
 
-let licz_odbicie (a,b,c) (x,y) = 
-    let liczpktwspolny (a,b,c) (a',b',c') =
-        let wsp_x = (b' *. c -. b *. c') /. (a' *. b -. a *. b')
-        and wsp_y = (a *. c' -. a' *. c) /. (a' *. b -. a *. b')
-        in (wsp_x,wsp_y)
-    in
-    let a' = -.b in let b' = a in let c' = -.a' *. x -. b' *. y
-    in let  x', y' = liczpktwspolny (a,b,c) (a',b',c')
-    in (2.*.(x'-.x) +. x, 2.*.(y'-.y) +. y) 
+let licz_odbicie (a, b, c) (x, y) = 
+    let licz_pkt_wspolny (a, b, c) (a2, b2, c2) = (*dla dwoch prostych Ax+By+C=0 oraz A2x+B2y+C2=0*)
+        let wsp_x = (b2 *. c -. b *. c2) /. (a2 *. b -. a *. b2)
+        and wsp_y = (a *. c2 -. a2 *. c) /. (a2 *. b -. a *. b2)
+        in (wsp_x, wsp_y) in 
+    (* liczymy wspolczynniki prostej prostopadlej do Ax+By+C=0 przechodzacej przez x i y*)
+    let a_prost = 0. -. b in 
+    let b_prost = a in 
+    let c_prost = 0. -. a_prost *. x -. b_prost *. y in 
+    let  x_wsp, y_wsp = licz_pkt_wspolny (a, b, c) (a_prost, b_prost, c_prost)
+    in (2. *. (x_wsp -. x) +. x, 2.*.(y_wsp -. y) +. y) 
 
 let do_drugiej x = x*.x;;
 (*================================= INTERFACE FUNCTIONS =================================*)
-let prostokat ((p1x,p1y):point) ((p2x,p2y):point) =
-    let pom (x,y) =
-        if x < p1x || x > p2x then 0
-        else if y < p1y ||y > p2y then 0
+let prostokat ((p1x, p1y):point) ((p2x, p2y):point) =
+    fun (x, y) ->
+        if x < p1x || x > p2x || y < p1y ||y > p2y then 0
         else 1
-    in  (pom:kartka)
 
-let kolko ((px,py):point) r =
-    let pom (x,y) =
+let kolko ((px, py):point) r =
+    fun (x, y) ->
         if Float.sqrt (do_drugiej (px-.x) +. do_drugiej (y-.py)) >  r then 0
         else 1
-    in  (pom:kartka)
 
-let zloz ((p1x,p1y):point) ((p2x,p2y):point) (given_kartka:kartka) =
+let zloz ((p1x, p1y):point) ((p2x, p2y):point) (given_kartka:kartka) =
     (* Ax + By + C = 0*)
-    let a = p1y -. p2y and b = p2x -. p1x in let c = -.a *. p1x -. b *. p1y 
-    in 
-        let pom (x,y) =
-        match prawolewona (p2x-.p1x, p2y-.p1y) (x-.p1x, y-.p1y) with
-        | Prawo -> 0
-        | Na -> given_kartka (x,y)
-        | Lewo ->
-            let odbity_x,odbity_y = licz_odbicie (a,b,c) (x,y)
-            in 
-            ( given_kartka (x,y) + (given_kartka (odbity_x,odbity_y)) )
-    in (pom: kartka)
-
+    let a = p1y -. p2y and b = p2x -. p1x in let c = -.a *. p1x -. b *. p1y in 
+        fun (x, y) ->
+            match prawo_lewo_na (p2x -. p1x, p2y -. p1y) (x -. p1x, y -. p1y) with
+            | Prawo -> 0
+            | Na -> given_kartka (x, y)
+            | Lewo ->
+                let odbity_x,odbity_y = licz_odbicie (a, b, c) (x, y)
+                in ( given_kartka (x, y) + (given_kartka (odbity_x, odbity_y)) )
 
 let skladaj li given_kartka =
-    let pom = List.fold_left (fun a (point1, point2)-> zloz point1 point2 a) given_kartka li
-    in (pom: kartka) 
+    List.fold_left (fun a (point1, point2)-> zloz point1 point2 a) given_kartka li
 ;;
