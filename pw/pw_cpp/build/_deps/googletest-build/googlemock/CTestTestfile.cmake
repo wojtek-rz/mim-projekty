85a29b7119fb3a318c96a2a3bf321d43882efb95
Wojciech Rzepliński <w.r.mirror@gmail.com>
2023-01-28 13:17:43 +0100

    usunieto zbedne pliki

--- pw/pw_cpp/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wojtek/mim-projekty/pw/pw_cpp/build/_deps/googletest-src/googlemock
-# Build directory: /home/wojtek/mim-projekty/pw/pw_cpp/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- pw/pw_cpp/build/my_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/wojtek/mim-projekty/pw/pw_cpp/build/my_tests[1]_tests.cmake")
-  include("/home/wojtek/mim-projekty/pw/pw_cpp/build/my_tests[1]_tests.cmake")
-else()
-  add_test(my_tests_NOT_BUILT my_tests_NOT_BUILT)
-endif()
